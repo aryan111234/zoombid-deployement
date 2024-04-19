fa0bea244740bb54ae2c04505605a96ad9809036
Aryan <aryandhakal92@gmail.com>
2024-04-19 21:14:40 +0545

    dep config

--- server.js
+++ server.js
@@ -16,5 +16,14 @@ app.use("/api/products", productsRoute);
 app.use("/api/bids", bidsRoute);
 app.use("/api/notifications", notificationsRoute);
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(port, () => console.log(`Node/Express Server started on port ${port}`));
 
